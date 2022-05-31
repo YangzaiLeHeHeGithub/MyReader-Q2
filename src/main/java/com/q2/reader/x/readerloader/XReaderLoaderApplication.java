f85fe6ca8cd84fbc5d508c38e30c0b9f4e8e99fd
提着月亮去火星 <1.qq.com>
2022-05-31 16:04:50 +0800

    add:initialize project

---
+++ src/main/java/com/q2/reader/x/readerloader/XReaderLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.q2.reader.x.readerloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XReaderLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XReaderLoaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/q2/reader/x/readerloader/XReaderLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.q2.reader.x.readerloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XReaderLoaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
